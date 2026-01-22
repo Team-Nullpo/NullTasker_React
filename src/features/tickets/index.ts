bab31fe3d0e2dbfc8c88fe8fdd012f61a686101c
bit <mushbeen25@gmail.com>
2026-01-22 17:43:12 +0900

    refac: edit index.ts

--- src/features/tickets/index.ts
+++ src/features/tickets/index.ts
@@ -3,4 +3,3 @@ export { default as TicketCard } from "./components/TicketCard";
 export { default as TicketForm } from "./components/TicketForm";
 export { default as TicketList } from "./components/TicketList";
 export { default as TicketPage } from "./pages/TicketPage";
-export { default as ticketService } from "../../shared/services/ticketService";
