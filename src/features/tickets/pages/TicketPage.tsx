9c35ef05a1f6a65dcfdfae447fa2bec77028c25c
bit <mushbeen25@gmail.com>
2026-01-23 10:32:49 +0900

    feat: reload on creating ticket

--- src/features/tickets/pages/TicketPage.tsx
+++ src/features/tickets/pages/TicketPage.tsx
@@ -5,12 +5,21 @@ import { Modal } from "@mui/material";
 
 const TicketPage: React.FC = () => {
   const [isFormOpen, setIsFormOpen] = React.useState(false);
+  const [refreshKey, setRefreshKey] = React.useState(0);
+
   const openForm = () => setIsFormOpen(true);
+  const closeForm = () => setIsFormOpen(false);
+
+  const handleSave = () => {
+    closeForm();
+    setRefreshKey((prev) => prev + 1);
+  };
+
   return (
     <div>
-      <TicketList onCreateClick={openForm} />
-      <Modal open={isFormOpen} onClose={() => setIsFormOpen(false)}>
-        <TicketForm />
+      <TicketList key={refreshKey} onCreateClick={openForm} />
+      <Modal open={isFormOpen} onClose={closeForm}>
+        <TicketForm onSave={handleSave} onCancel={closeForm} />
       </Modal>
     </div>
   );
